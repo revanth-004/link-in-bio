8bddddb6ccf9ff56629f96424e7c5642e0252c33
Revanth kumar S <revanthsureshlearn@gmail.com>
2024-11-22 22:45:07 +0530

    resume button

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      bg: {
+        dark: "#60a5fa",
+        thick: "#91c3fd",
+        light: "#bfdbfe",
+        thin: "#dbeafe",
+      },
       backdropBlur: {
-        'none': 'none',
-        'sm': '4px',
-        'md': '8px',
-        'lg': '16px',
-        'xl': '24px',
-      }
+        none: "none",
+        sm: "4px",
+        md: "8px",
+        lg: "16px",
+        xl: "24px",
+      },
     },
   },
   plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/aspect-ratio'),
-    require('tailwind-scrollbar'),
-    require('tailwindcss-filters'),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/aspect-ratio"),
+    require("tailwind-scrollbar"),
+    require("tailwindcss-filters"),
   ],
-  
-}
+};
